6dc387b8e54260ef8cea1e12fcf0e795c9b085cb
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2025-06-04 12:33:02 -0600

    added url for past-orders

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import react_app_view, user_product_list, discount_options, login_view, logout_view
+from .views import react_app_view, user_product_list, discount_options, login_view, logout_view, user_past_orders
 
 urlpatterns = [
     path('api/user-product-list/', user_product_list),
     path('api/discount-options/', discount_options),
     path('api/login/', login_view, name='login'),
     path('api/logout/', logout_view, name='logout'),
+    path('api/past-orders/', user_past_orders, name='past_orders'),
     path('', react_app_view, name='home'),
 ]
